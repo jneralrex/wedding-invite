fa7b4f51586bedbbe5cb296c83f83627671ac3be
Idah Abubakar Isah <idahabubakar49@gmail.com>
2024-08-02 08:10:38 +0100

    Updating media query for tablet screen

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import GalleryHeader from '../Components/GalleryHeader'
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
+import '../Assets/Styles/Gallery.css'
 
 
 const Gallery = () => {
@@ -24,12 +25,66 @@ const Gallery = () => {
       className="mb-3"
       fill
     >
-      <Tab eventKey="home" title="PROPOSAL">
+      <Tab eventKey="home" title="PROPOSAL IMAGES">
         Tab content for Proposal
+      <div className="image-holder-outter">
+         <div className="image-holder-inner">
+          <img src="" alt="" />
+         </div>
+         <div className="image-holder-inner">
+          <img src="" alt="" />
+         </div><div className="image-holder-inner">
+          <img src="" alt="" />
+         </div><div className="image-holder-inner">
+          <img src="" alt="" />
+         </div><div className="image-holder-inner">
+          <img src="" alt="" />
+         </div><div className="image-holder-inner">
+          <img src="" alt="" />
+         </div><div className="image-holder-inner">
+          <img src="" alt="" />
+         </div>
+         <div className="image-holder-inner">
+          <img src="" alt="" />
+         </div>
+         <div className="image-holder-inner">
+          <img src="" alt="" />
+         </div>
+         <div className="image-holder-inner">
+          <img src="" alt="" />
+         </div>
+      </div>
       </Tab>
    
-      <Tab eventKey="longer-tab" title="INTRODUCTION">
+      <Tab eventKey="longer-tab" title="INTRODUCTION IMAGES">
         Tab content for Introduction
+        <div className="image-holder-outter">
+         <div className="image-holder-inner">
+          <img src="" alt="" />
+         </div>
+         <div className="image-holder-inner">
+          <img src="" alt="" />
+         </div><div className="image-holder-inner">
+          <img src="" alt="" />
+         </div><div className="image-holder-inner">
+          <img src="" alt="" />
+         </div><div className="image-holder-inner">
+          <img src="" alt="" />
+         </div><div className="image-holder-inner">
+          <img src="" alt="" />
+         </div><div className="image-holder-inner">
+          <img src="" alt="" />
+         </div>
+         <div className="image-holder-inner">
+          <img src="" alt="" />
+         </div>
+         <div className="image-holder-inner">
+          <img src="" alt="" />
+         </div>
+         <div className="image-holder-inner">
+          <img src="" alt="" />
+         </div>
+      </div>
       </Tab>
 
     </Tabs>
